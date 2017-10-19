4ffe9ad2b231bdbc6c2d3068c2a0473d2672727a
Tac <fmessia@student.42.fr>
2017-10-19 17:42:57 +0200

    changed the frame name

--- src/init.c
+++ src/init.c
@@ -10,22 +10,34 @@ void		set_hooks(t_env *e)
 	mlx_loop(e->mlx);
 }
 
-void		scene_init(t_env *e)
+t_object	*load_scene(t_env *e)
+{
+	t_object	*res;
+	if (!(res = malloc(sizeof(t_object) * 4)))
+		s_error("\x1b[2;31mCan't create scene array\x1b[0m");
+	res[0]
+}
+
+void		frame_init(t_env *e)
 {
 	int		bpp;
 	int		row;
 	int		endian;
 
-	if (!(e->scene = malloc(sizeof(t_frame))))
-		s_error("\x1b[2;31mCan't initialize the scene\x1b[0m");
-	e->scene->w = e->win_w;
-	e->scene->h = e->win_h;
+	if (!(e->frame = malloc(sizeof(t_frame))))
+		s_error("\x1b[2;31mCan't initialize the frame\x1b[0m");
+	e->frame->w = e->win_w;
+	e->frame->h = e->win_h;
 //	a proteger les 2 instructions en dessous
-	e->scene->ptr = mlx_new_image(e->mlx, e->scene->w, e->scene->h);
-	e->scene->pix = mlx_get_data_addr(e->scene->ptr, &(bpp), &(row), &(endian));
-	e->scene->bpp = bpp;
-	e->scene->row = row;
-	e->scene->endian = endian;
+	if (!(e->frame->ptr = mlx_new_image(e->mlx, e->frame->w, e->frame->h)))
+		s_error("\x1b[2;31mCan't create new mlx image\x1b[0m");
+	if (!(e->frame->pix = mlx_get_data_addr(e->frame->ptr, \
+							&(bpp), &(row), &(endian))))
+		s_error("\x1b[2;31mCan't create get image address\x1b[0m");
+	e->frame->bpp = bpp;
+	e->frame->row = row;
+	e->frame->endian = endian;
+//	e->scene = load_scene(t_env *e);
 }
 
 void		init(t_env *e)
@@ -36,20 +48,20 @@ void		init(t_env *e)
 	e->cen_x = e->win_w / 2;
 	e->cen_y = e->win_h / 2;
 	e->mou_x = 0;
+	e->mou_y = 0;
 	e->mvx = 0;
 	e->mvy = 0;
-	e->mvz = 0;
+	e->mvz = 0;	
+	e->gpu = IS_GPU;
 
 	e->run = 1;
 
-	e->mou_y = 0;
-	e->gpu = IS_GPU;
 	ft_bzero(&e->keys, sizeof(t_key));
 	if (!(e->mlx = mlx_init()))
 		s_error("\x1b[2;31mError can't initialize minilibx\x1b[0m");
 	if (!(e->win = mlx_new_window(e->mlx, e->win_w, e->win_h, "RTv1")))
 		s_error("\x1b[2;31mError minilibx window creation failed\x1b[0m");
-	scene_init(e);
+	frame_init(e);
 	if (opencl_init(e) != 0)
 	{
 		e->gpu = 0;
