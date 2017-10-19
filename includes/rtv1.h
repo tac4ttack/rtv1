cc1c56944917b61efbd319cc81a8f4b7d466723b
Tac <fmessia@student.42.fr>
2017-10-19 16:09:32 +0200

    added the object types structures

--- includes/rtv1.h
+++ includes/rtv1.h
@@ -63,12 +63,63 @@ typedef struct			s_ray
 	float				t;
 }						t_ray;
 
+typedef struct			s_cam
+{
+	cl_float3			hor;
+	cl_float3			ver;
+	cl_float3			dir;
+	cl_float3			pos;
+}						t_cam;
+
+typedef struct			s_cone
+{
+	cl_float3			pos;
+	cl_float3			dir;
+	int					color;
+}						t_cone;
+
+typedef struct			s_cylinder
+{
+	cl_float3			pos;
+	cl_float3			dir;
+	float				radius;
+	int					color;
+}						t_cylinder;
+
+typedef struct			s_light
+{
+	char				type;
+	cl_float3			pos;
+	cl_float3			dir;
+	float				intensity;
+	int					color;
+}						t_light;
+
+typedef struct			s_plane
+{
+	cl_float3			pos;
+	cl_float3			normale;
+	int					color;
+}						t_plane;
+
 typedef struct			s_sphere
 {
-	t_v3f				center;
 	float				radius;
-	int					hue;
+	cl_float3			pos;
+//	cl_float3			dir;
+	int					color;
 }						t_sphere;
+
+typedef struct			s_object
+{
+	char				type;
+	t_cam				cam;
+	t_cone				cone;
+	t_cylinder			cylinder;
+	t_light				light;
+	t_plane				plane;
+	t_sphere			sphere;
+}						t_object;
 
 typedef struct			s_frame
 {
