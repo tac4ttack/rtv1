7f07dee13f71e72e663b1b001a26e411174e7698
Tac <fmessia@student.42.fr>
2017-10-19 22:37:08 +0200

    fixed hud leaks

--- src/mlx_main_loop.c
+++ src/mlx_main_loop.c
@@ -1,5 +1,22 @@
 #include "rtv1.h"
 
+void	display_hud(t_env *e)
+{
+	char *mvtx;
+	char *mvty;
+	char *mvtz;
+
+	mvtx = ft_ftoa(e->mvt.x);
+	mvty = ft_ftoa(e->mvt.y);
+	mvtz = ft_ftoa(e->mvt.z);
+	mlx_string_put(e->mlx, e->win, 10, 10, 0x00ffffff, mvtx);
+	mlx_string_put(e->mlx, e->win, 10, 25, 0x00ffffff, mvty);
+	mlx_string_put(e->mlx, e->win, 10, 40, 0x00ffffff, mvtz);
+	free(mvtx);
+	free(mvty);
+	free(mvtz);
+}
+
 void	refresh(t_env *e)
 {
 	if (e)
@@ -7,9 +24,7 @@ void	refresh(t_env *e)
 		mlx_clear_window(e->mlx, e->win);
 		draw(e);
 		mlx_put_image_to_window(e->mlx, e->win, e->frame->ptr, 0, 0);
-		mlx_string_put(e->mlx, e->win, 10, 10, 0x00ffffff, ft_ftoa(e->mvt.x));
-		mlx_string_put(e->mlx, e->win, 10, 25, 0x00ffffff, ft_ftoa(e->mvt.y));
-		mlx_string_put(e->mlx, e->win, 10, 40, 0x00ffffff, ft_ftoa(e->mvt.z));
+		display_hud(e);
 		e->run = 0;
 	}
 }
