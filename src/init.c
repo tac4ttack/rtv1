1559a1413ff14e37a6ae53cee75f1d148c5f33e5
Tac <fmessia@student.42.fr>
2017-10-19 18:04:01 +0200

    premade the scene object array

--- src/init.c
+++ src/init.c
@@ -10,12 +10,68 @@ void		set_hooks(t_env *e)
 	mlx_loop(e->mlx);
 }
 
-t_object	*load_scene(t_env *e)
+t_object	*load_scene(void)
 {
 	t_object	*res;
 	if (!(res = malloc(sizeof(t_object) * 4)))
 		s_error("\x1b[2;31mCan't create scene array\x1b[0m");
-	res[0]
+	
+	//camera
+	ft_bzero(&res[0], sizeof(t_object));
+	res[0].type = 0;
+	res[0].id = 0;
+	res[0].cam.pos.x = -20;
+	res[0].cam.pos.y = 0;
+	res[0].cam.pos.z = 50;
+	res[0].cam.dir.x = 0;
+	res[0].cam.dir.y = 0;
+	res[0].cam.dir.z = 1;
+	res[0].cam.hor.x = 0.6;
+	res[0].cam.hor.y = 0;
+	res[0].cam.hor.z = 0;
+	res[0].cam.ver.x = 0;
+	res[0].cam.ver.y = 0.6;
+	res[0].cam.ver.z = 0;
+
+	//plane
+	ft_bzero(&res[1], sizeof(t_object));
+	res[1].type = 4;
+	res[1].id = 1;
+	res[1].plane.pos.x = 0;
+	res[1].plane.pos.y = 0;
+	res[1].plane.pos.z = 0;
+	res[1].plane.normale.x = 0;
+	res[1].plane.normale.y = 1;
+	res[1].plane.normale.z = 0;
+	res[1].plane.color = 0x00ff0000;
+
+	//sphere 1
+	ft_bzero(&res[2], sizeof(t_object));
+	res[2].type = 5;
+	res[2].id = 2;
+	res[2].sphere.pos.x = 0;
+	res[2].sphere.pos.y = 0;
+	res[2].sphere.pos.x = 0;
+//	res[2].sphere.dir.x = 0;
+//	res[2].sphere.dir.y = 0;
+//	res[2].sphere.dir.z = 0;
+	res[2].sphere.radius = 5;
+	res[2].sphere.color = 0x0000ff00;
+
+	//sphere 2
+	ft_bzero(&res[3], sizeof(t_object));
+	res[3].type = 5;
+	res[3].id = 3;
+	res[3].sphere.pos.x = 0;
+	res[3].sphere.pos.y = 0;
+	res[3].sphere.pos.x = 0;
+//	res[3].sphere.dir.x = 0;
+//	res[3].sphere.dir.y = 0;
+//	res[3].sphere.dir.z = 0;
+	res[3].sphere.radius = 10;
+	res[3].sphere.color = 0x000000ff;
+
+	return (res);
 }
 
 void		frame_init(t_env *e)
@@ -28,7 +84,6 @@ void		frame_init(t_env *e)
 		s_error("\x1b[2;31mCan't initialize the frame\x1b[0m");
 	e->frame->w = e->win_w;
 	e->frame->h = e->win_h;
-//	a proteger les 2 instructions en dessous
 	if (!(e->frame->ptr = mlx_new_image(e->mlx, e->frame->w, e->frame->h)))
 		s_error("\x1b[2;31mCan't create new mlx image\x1b[0m");
 	if (!(e->frame->pix = mlx_get_data_addr(e->frame->ptr, \
@@ -37,7 +92,7 @@ void		frame_init(t_env *e)
 	e->frame->bpp = bpp;
 	e->frame->row = row;
 	e->frame->endian = endian;
-//	e->scene = load_scene(t_env *e);
+	e->scene = load_scene();
 }
 
 void		init(t_env *e)
