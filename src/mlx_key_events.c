21ce81be98efddea314469fca33f3fd3fd223681
Tac <fmessia@student.42.fr>
2017-11-11 17:38:01 +0100

    added a free to avoid leak with the hud print

--- src/mlx_key_events.c
+++ src/mlx_key_events.c
@@ -4,9 +4,9 @@ void	reset_cam_pos(t_env *e)
 {
 	if (e)
 	{
-		e->mvt.x = 0;
-		e->mvt.y = 0;
-		e->mvt.z = 0;
+		e->param.mvt.x = 0;
+		e->param.mvt.y = 0;
+		e->param.mvt.z = 0;
 		e->param.bloom = 1.80;
 //		e->rtx = 0;
 //		e->rty = 0;
@@ -19,11 +19,13 @@ void			mlx_keyboard_repeated(t_env *e)
 	if (!e)
 		exit(EXIT_FAILURE);
 	(KP_ESC ? quit(e) : 0);
-	(KP_Q ? quit(e) : 0);
-	(KP_W ? e->mvt.z += 0.1 : 0);
-	(KP_S ? e->mvt.z -= 0.1 : 0);
-	(KP_A ? e->mvt.x -= 0.1 : 0);
-	(KP_D ? e->mvt.x += 0.1 : 0);
+	(KP_W ? e->param.mvt.z += 0.1 : 0);
+	(KP_S ? e->param.mvt.z -= 0.1 : 0);
+	(KP_A ? e->param.mvt.x -= 0.1 : 0);
+	(KP_D ? e->param.mvt.x += 0.1 : 0);
+	(KP_SPC ? e->param.mvt.y -= 0.1 : 0);
+	(KP_C ? e->param.mvt.y += 0.1 : 0);
+	(KP_R ? reset_cam_pos(e) : 0);
 	(KP_NPLU ? e->param.bloom += 0.1 : 0);
 	(KP_NMIN ? e->param.bloom -= 0.1 : 0);
 	if (KP_N4 || KP_N6 || KP_N8 || KP_N5 || KP_N7 || KP_N9)
@@ -36,7 +38,4 @@ void			mlx_keyboard_repeated(t_env *e)
 		(KP_N9 ? e->lights[0].pos.z += 0.1 : 0);
 		opencl_allocate_scene_memory(e);
 	}
-	(KP_SPC ? e->mvt.y -= 0.1 : 0);
-	(KP_C ? e->mvt.y += 0.1 : 0);
-	(KP_R ? reset_cam_pos(e) : 0);
 }
