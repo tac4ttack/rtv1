bf054c327d81fc41c896a559162dfc7ba4104f8f
Tac <fmessina@student.42.fr>
2018-03-01 18:28:50 +0100

    normed

--- src/mlx_key_no_repeat.c
+++ src/mlx_key_no_repeat.c
@@ -6,13 +6,13 @@
 /*   By: fmessina <fmessina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/01 18:18:35 by fmessina          #+#    #+#             */
-/*   Updated: 2018/03/01 18:18:36 by fmessina         ###   ########.fr       */
+/*   Updated: 2018/03/01 18:25:26 by fmessina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
 
-void mlx_key_no_repeat_event(t_env *e, const int key)
+void	mlx_key_no_repeat_event(t_env *e, const int key)
 {
 	if (key == K_1)
 	{
